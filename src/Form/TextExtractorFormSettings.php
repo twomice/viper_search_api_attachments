69b59410f2678df49696c2835ce36d9769f3271f
izus <izus@514568.no-reply.drupal.org>
2015-09-07 09:38:41 +0200

    Issue #2470553 by izus: Change SafeMarkup::checkPlain to Html::escape.

--- src/Form/TextExtractorFormSettings.php
+++ src/Form/TextExtractorFormSettings.php
@@ -2,7 +2,7 @@
 
 namespace Drupal\search_api_attachments\Form;
 
-use Drupal\Component\Utility\SafeMarkup;
+use Drupal\Component\Utility\Html;
 use Drupal\Core\Config\ConfigFactoryInterface;
 use Drupal\Core\Form\ConfigFormBase;
 use Drupal\Core\Form\FormStateInterface;
@@ -147,8 +147,8 @@ class TextExtractorFormSettings extends ConfigFormBase {
       'descriptions' => array()
     );
     foreach ($this->textExtractorPluginManager->getDefinitions() as $plugin_id => $plugin_definition) {
-      $options['labels'][$plugin_id] = SafeMarkup::checkPlain($plugin_definition['label']);
-      $options['descriptions'][$plugin_id] = SafeMarkup::checkPlain($plugin_definition['description']);
+      $options['labels'][$plugin_id] = Html::escape($plugin_definition['label']);
+      $options['descriptions'][$plugin_id] = Html::escape($plugin_definition['description']);
     }
     return $options;
   }
