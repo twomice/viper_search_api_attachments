86de05178a8aba3c41d930cbc9f85fc0cd8261c7
Ismaeil Abouljamal <abouljamal.ismaeil@gmail.com>
2019-02-08 12:09:07 +0100

    Issue #2470553 by izus, Replace usage of the deprecated file_uri_scheme() function

--- src/TextExtractorPluginBase.php
+++ src/TextExtractorPluginBase.php
@@ -3,6 +3,7 @@
 namespace Drupal\search_api_attachments;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\File\FileSystemInterface;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Messenger\MessengerInterface;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
@@ -54,14 +55,22 @@ abstract class TextExtractorPluginBase extends PluginBase implements TextExtract
   protected $messenger;
 
   /**
+   * The file system service.
+   *
+   * @var \Drupal\Core\File\FileSystemInterface
+   */
+  protected $fileSystem;
+
+  /**
    * {@inheritdoc}
    */
-  public function __construct(array $configuration, $plugin_id, array $plugin_definition, ConfigFactoryInterface $config_factory, StreamWrapperManagerInterface $stream_wrapper_manager, MimeTypeGuesserInterface $mime_type_guesser, MessengerInterface $messenger) {
+  public function __construct(array $configuration, $plugin_id, array $plugin_definition, ConfigFactoryInterface $config_factory, StreamWrapperManagerInterface $stream_wrapper_manager, MimeTypeGuesserInterface $mime_type_guesser, MessengerInterface $messenger, FileSystemInterface $file_system) {
     parent::__construct($configuration, $plugin_id, $plugin_definition);
     $this->configFactory = $config_factory;
     $this->streamWrapperManager = $stream_wrapper_manager;
     $this->mimeTypeGuesser = $mime_type_guesser;
     $this->messenger = $messenger;
+    $this->fileSystem = $file_system;
   }
 
   /**
@@ -69,7 +78,7 @@ abstract class TextExtractorPluginBase extends PluginBase implements TextExtract
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
     return new static(
-        $configuration, $plugin_id, $plugin_definition, $container->get('config.factory'), $container->get('stream_wrapper_manager'), $container->get('file.mime_type.guesser'), $container->get('messenger')
+        $configuration, $plugin_id, $plugin_definition, $container->get('config.factory'), $container->get('stream_wrapper_manager'), $container->get('file.mime_type.guesser'), $container->get('messenger'), $container->get('file_system')
     );
   }
 
@@ -145,7 +154,7 @@ abstract class TextExtractorPluginBase extends PluginBase implements TextExtract
    */
   public function getRealpath($uri) {
     $wrapper = $this->streamWrapperManager->getViaUri($uri);
-    $scheme = file_uri_scheme($uri);
+    $scheme = $this->fileSystem->uriScheme($uri);
     $local_wrappers = $this->streamWrapperManager->getWrappers(StreamWrapperInterface::LOCAL);
     if (in_array($scheme, array_keys($local_wrappers))) {
       return $wrapper->realpath();
