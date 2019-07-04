8dacf72cf62d198aea42b8d6034eca9996451508
Ismaeil Abouljamal <iabouljamal@linagora.com>
2019-07-04 16:17:28 +0200

    by izus: Fix pareview.sh report

--- src/Plugin/search_api_attachments/TikaServerExtractor.php
+++ src/Plugin/search_api_attachments/TikaServerExtractor.php
@@ -80,7 +80,8 @@ class TikaServerExtractor extends TextExtractorPluginBase {
     $response = $this->httpClient->request('PUT', $this->getServerUri() . '/tika', $options);
     if ($response->getStatusCode() === 200) {
       $data = (string) $response->getBody();
-    }else{
+    }
+    else {
       throw new \Exception('Tika JAX-RS Server is not available.');
     }
 
